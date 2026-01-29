e92652e4e4c67295c92a5566f307477e2aa81789
ibizugbe001 <abraham@tradevu.co>
2026-01-29 23:32:26 +0100

    Fix issues with scroll up.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import "./App.css";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop";
+
 import Home from "./pages/Home";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
 import Merch from "./pages/Merch";
 import MerchShow from "./pages/MerchShow";
 import MerchOrderSuccess from "./components/merch/MerchOrderSuccess";
@@ -18,26 +15,30 @@ import Events from "./pages/Events";
 
 function App() {
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/merch" element={<Merch />} />
-          <Route path="/merch/:id" element={<MerchShow />} />
-          <Route path="/merch/order-success" element={<MerchOrderSuccess />} />
-          <Route path="/tickets/tradout" element={<TradoutTickets />} />
-          <Route path="/tickets/success" element={<TicketOrderSuccess />} />
-          <Route path="/events" element={<Events />} />
-          <Route path="/events/:eventId" element={<EventShow />} />
-          <Route
-            path="/tickets/tradout"
-            element={<Navigate to="/events/tradout" replace />}
-          />
-        </Routes>
-      </Router>{" "}
-    </>
+    <BrowserRouter>
+      <ScrollToTop />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+
+        <Route path="/merch" element={<Merch />} />
+        <Route path="/merch/:id" element={<MerchShow />} />
+        <Route path="/merch/order-success" element={<MerchOrderSuccess />} />
+
+        <Route path="/tickets/tradout" element={<TradoutTickets />} />
+        <Route path="/tickets/success" element={<TicketOrderSuccess />} />
+
+        <Route path="/events" element={<Events />} />
+        <Route path="/events/:eventId" element={<EventShow />} />
+
+        <Route
+          path="/tickets/tradout"
+          element={<Navigate to="/events/tradout" replace />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 

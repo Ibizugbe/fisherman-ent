0c64d7d76b73046ef6c24055a8052fa2e42f5201
ibizugbe001 <abraham@tradevu.co>
2026-02-05 21:58:13 +0100

    Add merch slide show.

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,13 +1,23 @@
 import merch1 from "../assets/merch/StayFishyTop.jpg";
 import merch2 from "../assets/merch/StayFishyTanktop.jpg";
 import merch3 from "../assets/merch/StayFishyHat.jpg";
+import merchAlt1 from "../assets/merch/merch1.jpeg";
+import merchAlt2 from "../assets/merch/merch2.jpeg";
+import merchAlt3 from "../assets/merch/merch3.jpeg";
+import merchAlt4 from "../assets/merch/merchh4.jpeg";
+import merchAlt5 from "../assets/merch/merch5.jpeg";
+import merchAlt6 from "../assets/merch/merch6.jpeg";
+import merchAlt7 from "../assets/merch/merch7.jpeg";
+import merchAlt8 from "../assets/merch/merch8.jpeg";
+import merchAlt9 from "../assets/merch/merch9.jpg";
 
 export const products = [
   {
     id: 1,
     name: "Stay Fishy Tee Stonewash",
     price: "NGN 40,000",
-    image: merch1,
+    image: merchAlt4,
+    images: [merchAlt4, merchAlt5, merchAlt8, merchAlt9],
     colours: ["#405F8D"],
     material: "100% cotton",
     rating: 4.9,
@@ -19,7 +29,8 @@ export const products = [
     id: 2,
     name: "Stay Fishy Tank Stonewash",
     price: "NGN 40,000",
-    image: merch2,
+    image: merchAlt3,
+    images: [merchAlt3, merchAlt6],
     colours: ["#405F8D"],
     material: "100% cotton",
     rating: 4.9,
@@ -31,7 +42,8 @@ export const products = [
     id: 3,
     name: "Stay Fishy Fisherman Hat",
     price: "NGN 30,000",
-    image: merch3,
+    image: merchAlt2,
+    images: [merchAlt2, merchAlt7],
     colours: ["#111827"],
     material: "100% cotton",
     rating: 4.9,
